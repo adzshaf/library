33b31adf18470fddf3369b37d7fbd463940b5c76
Shafiya Adzhani <adz.arsym@gmail.com>
2021-03-01 13:39:30 +0800

    [Shafiya] add spec for has_title that returns true

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,13 @@
 class Book
     attr_reader :isbn, :title, :author
-    
+
     def initialize(isbn, title, author)
         @isbn = isbn
         @title = title
         @author = author
+    end
+
+    def has_title(title)
+        true
     end
 end
--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,17 +1,24 @@
 require 'book'
 
 RSpec.describe Book do
-    context "initialize" do
-        let(:isbn) { 9780747532744 }
-        let(:title) { 'Harry Potter' }
-        let(:author) { 'J. K. Rowling' }
+    let(:isbn) { 9780747532744 }
+    let(:title) { 'Harry Potter' }
+    let(:author) { 'J. K. Rowling' }
 
-        subject { Book.new isbn, title, author }
-        
+    subject { Book.new isbn, title, author }
+
+    context "initialize" do
         it "should be able to get isbn, title, author" do
             expect(subject.isbn).to eq isbn
             expect(subject.title).to eq title
             expect(subject.author).to eq author
         end
     end 
+
+    context "has_title" do
+        it "returns true if has_title('Harry Potter') is called" do
+            result = subject.has_title('Harry Potter')
+            expect(result).to eq true
+        end
+    end
 end
