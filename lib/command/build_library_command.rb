e785e8003b7ba3a088ad56c184ee96e574f11254
Shafiya Adzhani <adz.arsym@gmail.com>
2021-03-02 18:16:02 +0800

    [Shafiya] add spec for build library command

---
+++ lib/command/build_library_command.rb
@@ -0,0 +1,12 @@
+require_relative 'command'
+require_relative '../library.rb'
+
+class BuildLibraryCommand < Command
+    def execute(args)   
+        no_shelf = args[0]
+        row = args[1]
+        column = args[2]
+        
+        Library.instance.build_library(no_shelf, row, column)
+    end   
+end
---
+++ spec/command/build_command_spec.rb
@@ -0,0 +1,14 @@
+require 'command/build_library_command'
+require 'library'
+
+RSpec.describe BuildLibraryCommand do
+    context "execute" do
+        subject { BuildLibraryCommand.new }
+
+        it "should call library#build_library" do
+            args = [1, 1, 1]
+            expect_any_instance_of(Library).to receive(:build_library).with(args[0], args[1], args[2]).and_return("Shelf 1 with 1 row and 1 column is added")
+            expect(subject.execute(args)).to eq "Shelf 1 with 1 row and 1 column is added"
+        end
+    end
+end
